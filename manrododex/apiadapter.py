2b51ff9187e8c10c682304740952c203edf6059a
Charbel Assaad <sydiepus@gmail.com>
2022-08-22 20:46:07 +0300

    Fixed the rate checker. time was not preserved, more testing needed.

--- manrododex/apiadapter.py
+++ manrododex/apiadapter.py
@@ -10,27 +10,6 @@ from manrododex.exceptions import ResultNotOk
 
 API_URL = "https://api.mangadex.org"
 
-last_time = None
-
-
-def can_i_mauwku_requesto_senpai():
-    global last_time
-    current_time = time()
-    if last_time:
-        delta = current_time - last_time
-        logging.debug("Time since last request '%s'", str(delta))
-        if delta >= 0.2:
-            return
-        else:
-            sleep_for = 0.2 - delta
-            logging.debug("Waiting '%s'", str(sleep_for))
-            sleep(sleep_for)
-            last_time = current_time
-            return
-    else:
-        last_time = current_time
-        return
-
 
 class ApiAdapter:
     """Class to make the requests better ?
@@ -42,12 +21,31 @@ class ApiAdapter:
                     status_forcelist=[500, 502, 503, 504])
     session.mount('http://', HTTPAdapter(max_retries=retries))
     session.mount('https://', HTTPAdapter(max_retries=retries))
+    lastu_taimo = None
+
+    @classmethod
+    def can_i_mauwku_requesto_senpai(cls):
+        cuwuent_taimo = time()
+        if cls.lastu_taimo:
+            deuwuta = cuwuent_taimo - cls.lastu_taimo
+            logging.debug("Time since last request '%s'", str(deuwuta))
+            if deuwuta >= 0.2:
+                return
+            else:
+                suweep_fuwu = 0.2 - deuwuta
+                logging.debug("Waiting '%s'", str(suweep_fuwu))
+                sleep(suweep_fuwu)
+                cls.lastu_taimo = cuwuent_taimo
+                return
+        else:
+            cls.lastu_taimo = cuwuent_taimo
+            return
 
     # future me : Class methods are different -- they are called by a class, which is passed to the cls parameter of
     # the method. (Sololearn)
     @classmethod
     def make_request(cls, method, endpoint, passed_params=None, passed_headers=None):
-        can_i_mauwku_requesto_senpai()
+        cls.can_i_mauwku_requesto_senpai()
         req = cls.session.request(method, f"{API_URL}{endpoint}", params=passed_params, headers=passed_headers)
         if req.status_code == 200:
             logging.debug("Request successful.")
