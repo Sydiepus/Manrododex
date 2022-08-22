4debe4ca1d8abd5f632dbc4e1f1a1a72b27c5bf6
Charbel Assaad <sydiepus@gmail.com>
2022-08-22 20:49:29 +0300

    chapter path is now saved. added a new function to return img path.

--- manrododex/system_helper.py
+++ manrododex/system_helper.py
@@ -5,6 +5,7 @@ class SysHelper:
     def __init__(self, main_path, manga_title):
         self.main_path = main_path
         self.manga_path = path.join(self.main_path, manga_title)
+        self.chapter_path = None
 
     def create_main_manga_dir(self):
         if not path.exists(self.main_path):
@@ -15,6 +16,9 @@ class SysHelper:
             makedirs(self.manga_path)
 
     def create_chapter_dir(self, chapter_name):
-        chapter_path = path.join(self.manga_path, chapter_name)
-        if not path.exists(chapter_path):
-            makedirs(chapter_path)
+        self.chapter_path = path.join(self.manga_path, chapter_name)
+        if not path.exists(self.chapter_path):
+            makedirs(self.chapter_path)
+
+    def forge_img_path(self, img_name, img_ext):
+        return path.join(self.chapter_path, f"{img_name}{img_ext}")
